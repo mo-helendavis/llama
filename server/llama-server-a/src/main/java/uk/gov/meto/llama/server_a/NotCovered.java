af0438b8fad8d9219ece18ddf26b1bfd0388c6fb
helen.davis <helen.davis@metoffice.gov.uk>
2022-09-21 21:56:07 +0100

    add spotbugs, junit

--- server/llama-server-a/src/main/java/uk/gov/meto/llama/server_a/NotCovered.java
+++ server/llama-server-a/src/main/java/uk/gov/meto/llama/server_a/NotCovered.java
@@ -1,6 +1,9 @@
 package uk.gov.meto.llama.server_a;
 
 public class NotCovered {
+
+    // Trying to get something to show up on spotbugs
+    int unreadField = 4;
     int getValue() {
         return 4;
     }
--- server/llama-server-a/src/test/java/uk/gov/meto/llama/server_a/ExampleTest.java
+++ server/llama-server-a/src/test/java/uk/gov/meto/llama/server_a/ExampleTest.java
@@ -3,6 +3,7 @@ package uk.gov.meto.llama.server_a;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class ExampleTest {
     @Test
@@ -10,5 +11,12 @@ class ExampleTest {
         // Test getString, don't test length since we want a method with no test coverage
         Example example = new Example("TEST");
         assertEquals("TEST",example.getString());
+    }
+
+    @Test
+    public void getString2() {
+        // Test getString, don't test length since we want a method with no test coverage
+        Example example = new Example("LLAMA NAME");
+        assertEquals("LLAMA NAME",example.getString());
     }
 }
