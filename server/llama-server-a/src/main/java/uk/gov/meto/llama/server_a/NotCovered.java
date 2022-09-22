fca33704ed4f8c416919960c691f362df4258475
helen.davis <helen.davis@metoffice.gov.uk>
2022-09-22 18:34:09 +0100

    Add javadoc

--- server/llama-server-a/src/main/java/uk/gov/meto/llama/server_a/NotCovered.java
+++ server/llama-server-a/src/main/java/uk/gov/meto/llama/server_a/NotCovered.java
@@ -1,5 +1,8 @@
 package uk.gov.meto.llama.server_a;
 
+/**
+ * Class to be an example of a class not covered by unit testing
+ */
 public class NotCovered {
 
     // Trying to get something to show up on spotbugs
