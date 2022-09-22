828e3503c913e45696ca0c8852967fba240a3488
helen.davis <helen.davis@metoffice.gov.uk>
2022-09-22 18:38:48 +0100

    Remove unused field

--- server/llama-server-a/src/main/java/uk/gov/meto/llama/server_a/NotCovered.java
+++ server/llama-server-a/src/main/java/uk/gov/meto/llama/server_a/NotCovered.java
@@ -4,9 +4,6 @@ package uk.gov.meto.llama.server_a;
  * Class to be an example of a class not covered by unit testing
  */
 public class NotCovered {
-
-    // Trying to get something to show up on spotbugs
-    int unreadField = 4;
     int getValue() {
         return 4;
     }
